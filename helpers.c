27c69ddfeabc0e7c4db6c7d64bea0304e3b11333
thepinkpandesal <thepinkpandesal@users.noreply.github.com>
2023-07-15 11:04:42 +0800

    automated commit by submit50

--- helpers.c
+++ helpers.c
@@ -3,6 +3,7 @@
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
+    // Iterate over [i][j]
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)

bbec094587468d00a2fedcc5856cf3c93922c9ec
thepinkpandesal <thepinkpandesal@users.noreply.github.com>
2023-07-28 21:46:41 +0800

    automated commit by check50 [check50=True]

--- bank.py
+++ bank.py
@@ -1,8 +1,8 @@
 greeting = input("Greeting: ")
 
-if "hello" in greeting.lower():
+if greeting.lower().startswith("hello"):
     print("$0")
-elif "h" in greeting.lower():
+elif greeting[0].lower() == "h":
     print("$20")
 else:
     print("$100")

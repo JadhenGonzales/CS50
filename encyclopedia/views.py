f9aef648755d681c4f6202174373c1e1ae6dfa7f
Jadhen Nicolo Gonzales <jadhen.g@gmail.com>
2024-01-03 16:01:41 +0800

    index pages and links

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-
 from . import util
+from markdown2 import markdown
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def entry(request, name):
+    md_entry = util.get_entry(name)
+
+    return render(request, "encyclopedia/entry.html", {
+        "name": name,
+        "entry": markdown(md_entry),
     })
 

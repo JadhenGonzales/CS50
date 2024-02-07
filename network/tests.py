a8115880ecc37c17368ba558b40a81a7eae7d193
Jadhen Nicolo Gonzales <jadhen.g@gmail.com>
2024-02-07 11:15:33 +0800

    Added comprehensive tests for like_post

--- network/tests.py
+++ network/tests.py
@@ -28,28 +28,30 @@ class Login_Redirect_Test(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('login') + '?next=' + reverse('create_post'))
 
-class Create_Post_View_Test(TestCase):
+
+class Create_Post_Test(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
+
+        # Create and login User
         self.user = User.objects.create_user(username='testuser1', email='test@example.com', password='12345@Test')
         self.profile = Profile.objects.get(user=self.user)
-
-    def test_create_post(self):
-        """Test submitting post to create_post_view"""
-        #Log in User
         self.client.login(username='testuser1', password='12345@Test')
 
-        #Submit POST request to view
+    def test_create(self):
+        """Test submitting post to create_post"""
+
+        # Submit POST request to view
         request_data = {
             'post_content': 'Test post'
         }
         request = self.factory.post(reverse('create_post'), data=request_data)
         request.user = self.user
-        response = views.create_post_view(request)
+        response = views.create_post(request)
 
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, 201)
 
-        #Check if comment exists in database
+        # Check if comment exists in database
         post_exists = Post.objects.filter(
             owner=self.profile,
             text='Test post',
@@ -57,8 +59,133 @@ class Create_Post_View_Test(TestCase):
 
         self.assertTrue(post_exists)
 
+    def test_other_method(self):
+        """Test submitting a post with GET method"""
+
+        # Submit POST request to view
+        request_data = {
+            'post_content': 'Test post'
+        }
+        request = self.factory.get(reverse('create_post'), data=request_data)
+        request.user = self.user
+        response = views.create_post(request)
+
+        self.assertEqual(response.status_code, 400)
 
 
+class Like_Post_Test(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+        # Create User
+        self.user1 = User.objects.create_user(username='testuser1', email='test1@example.com', password='12345@Test')
+        self.profile = Profile.objects.get(user=self.user1)        
+        self.user2 = User.objects.create_user(username='testuser2', email='test2@example.com', password='12345@Test')
+
+        # Create test post
+        new_post = Post(owner=self.profile, text="Test post.")
+        new_post.save()
+        self.post_id = Post.objects.first().id
+
+    def test_like_post(self):
+        """Test liking a post."""
+
+        # Create request
+        self.client.login(username='testuser2', password='12345@Test')
+        request_data = {
+            'post_id': self.post_id,
+            'like_status': True
+        }
+        request = self.factory.post(reverse('like_post'), request_data, content_type='application/json')
+        request.user = self.user2
+        
+        # Submit to like_post view and assert response
+        response = views.like_post(request)
+        self.assertEqual((response.status_code), (200), response.content)
+
+        # Check if like saved in database
+        target_post = Post.objects.get(pk=self.post_id)
+        self.assertEqual(target_post.likes.count(), 1)
+
+    def test_like_own(self):
+        """Test liking your own post."""
+        
+        # Create request
+        self.client.login(username='testuser1', password='12345@Test')
+        request_data = {
+            'post_id': self.post_id,
+            'like_status': True
+        }
+        request = self.factory.post(reverse('like_post'), request_data, content_type='application/json')
+        request.user = self.user1
+        
+        # Submit to like_post view and assert response
+        response = views.like_post(request)
+        self.assertEqual(response.status_code, 400, response.content)
+
+        # Check if like not saved in database
+        target_post = Post.objects.get(pk=self.post_id)
+        self.assertEqual(target_post.likes.count(), 0)
+
+    def test_unlike(self):
+        """Test unliking an already liked post."""
+
+        # Add user 2 to likes
+        sample_post = Post.objects.get(pk=self.post_id)
+        sample_post.likes.add(self.user2.profile)
+        sample_post.save()
+
+        # Create request
+        self.client.login(username='testuser2', password='12345@Test')
+        request_data = {
+            "post_id": self.post_id,
+            "like_status": False
+        }
+        request = self.factory.post(reverse('like_post'), request_data, content_type='application/json')
+        request.user = self.user2
+        
+        # Submit to like_post view and assert response
+        response = views.like_post(request)
+        self.assertEqual(response.status_code, 200, response.content)
+
+        # Check if like saved in database
+        target_post = Post.objects.get(pk=self.post_id)
+        self.assertEqual(target_post.likes.count(), 0)
+
+    def test_submit_invalid(self):
+        """Test submitting an invalid JSON"""
+        
+        # Create request
+        self.client.login(username='testuser2', password='12345@Test')
+        request_data = {
+            'foo':'bar',
+            'baz': True
+        }
+        request = self.factory.post(reverse('like_post'), request_data, content_type='application/json')
+        request.user = self.user2
+        
+        # Submit to like_post view and assert response
+        response = views.like_post(request)
+        self.assertEqual(response.status_code, 400, response.content)
+
+    def test_like_invalid(self):
+        """Test liking an invalid post id."""
+        
+        # Get pk of last post item
+        last_post = Post.objects.order_by('pk').last()
+
+        # Create request
+        self.client.login(username='testuser2', password='12345@Test')
+        request_data = {
+            'post_id': last_post.id + 1,
+            'like_status': True
+        }
+        request = self.factory.post(reverse('like_post'), request_data, content_type='application/json')
+        request.user = self.user2
+        
+        # Submit to like_post view and assert response
+        response = views.like_post(request)
+        self.assertEqual((response.status_code), (400), response.content)
     
 
 """
--- network/views.py
+++ network/views.py
@@ -1,6 +1,8 @@
+import json
+
 from django.contrib.auth import authenticate, decorators, login, logout
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
 
@@ -15,19 +17,61 @@ def index(request):
 
 
 @decorators.login_required
-def create_post_view(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            c_form = form.cleaned_data
-            new_post = Post(
-                owner=request.user.profile,
-                text=c_form['post_content'],
-            )
-            new_post.save()
-    
-    return HttpResponseRedirect(reverse("index"))
+def create_post(request):
+    # Creating a post must be via POST
+    if request.method != "POST":
+        return JsonResponse({"error": "POST request required."}, status=400)
 
+    #Check form validity and save post
+    form = PostForm(request.POST)
+    if form.is_valid():
+        c_form = form.cleaned_data
+        new_post = Post(
+            owner=request.user.profile,
+            text=c_form["post_content"],
+        )
+        new_post.save()
+        return JsonResponse({"message": "Post sent successfully"}, status=201)
+    else:
+        return JsonResponse({"error": "Invalid form content"}, status=400)
+
+
+@decorators.login_required
+def like_post(request):
+    # Liking a post must be via POST
+    if request.method != "POST":
+        return JsonResponse({"error": "POST request required."}, status=400)
+
+    # Check for post ID and like status
+    data = json.loads(request.body)
+    post_id = data.get("post_id")
+    like_status = data.get("like_status")
+    if post_id is None or like_status is None:
+        return JsonResponse({"error": "Incomplete submission"}, status=400)
+    
+    # Check if target post exists
+    try:
+        target_post = Post.objects.get(pk=post_id)
+    except Post.DoesNotExist:
+        return JsonResponse({"error": "Post does not exist."}, status=400)
+    
+    # Check if user does not own the post
+    if target_post.owner == request.user.profile:
+        return JsonResponse({"error": "Cannot like own post."}, status=400)
+    
+    # Like or Unlike post
+    if like_status:
+        target_post.likes.add(request.user.profile)
+        message = "Like added."
+    elif request.user.profile in target_post.likes.all():
+        target_post.likes.remove(request.user.profile)
+        message = "Like removed."
+    else:
+        message = "No changes."
+    
+    target_post.save()
+    return JsonResponse({"message": message}, status=200)
+    
 
 def login_view(request):
     if request.method == "POST":
